a200b9ac5c39dca913c2f4a55caf93a22469b2da
nyashbox <nyashbox@tutanota.com>
2024-09-28 15:10:37 +0300

    test: add Connection class tests

---
+++ tests/test_connection.cc
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include <guidemyride/connection.h>
+
+#include "mock_connection.h"
+
+TEST(Connection, connectionGuard) {
+    auto connection = MockConnection();
+
+    // Use separate scope
+    {
+        ConnectionGuard lock(connection);
+
+        EXPECT_TRUE(connection.isLocked)
+            << "ConnectionGuard should lock the connection";
+    }
+
+    EXPECT_FALSE(connection.isLocked)
+        << "When leaving the scope, ConnectionGuard should release the "
+           "connection";
+}
