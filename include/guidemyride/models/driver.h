e562e24c3bd24d28bf62c7bb6a49bee944081f7a
nyashbox <nyashbox@tutanota.com>
2024-10-16 23:11:23 +0300

    feat: declare Driver model class

---
+++ include/guidemyride/models/driver.h
@@ -0,0 +1,56 @@
+
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024 nyashbox
+
+#ifndef GUIDEMYRIDE_MODELS_DRIVER_H_
+#define GUIDEMYRIDE_MODELS_DRIVER_H_
+
+#pragma once
+
+#include <guidemyride/models/session.h>
+#include <guidemyride/transaction/transaction.h>
+
+using guidemyride::transaction::TransactionBase;
+
+namespace guidemyride::models {
+
+class Driver {
+  public:
+    Driver(const std::string &fullname, const std::string &password,
+           const std::string &phone);
+
+    template <typename T>
+    static Driver get(const T &value, TransactionBase &txn,
+                      const std::string_view field);
+
+    template <typename T>
+    static bool verifyPassword(const T &value, const std::string_view pass,
+                               TransactionBase &txn,
+                               const std::string_view field);
+
+    template <typename T>
+    static void updatePassword(const T &value, const std::string_view pass,
+                               TransactionBase &txn,
+                               const std::string_view field);
+
+    const std::string &getFullname(void) const { return mFullname; };
+    const std::string &getPhoneNumber(void) const { return mPhone; };
+    const std::string &getPassword(void) const { return mPassword; };
+
+    static void init(TransactionBase &txn);
+    operator Session() const;
+    void commit(TransactionBase &txn);
+
+  private:
+    Driver(const uint32_t id, const std::string &fullname,
+           const std::string &password, const std::string &phone);
+
+    uint32_t mId = 0;
+    std::string mFullname;
+    std::string mPassword;
+    std::string mPhone;
+};
+
+} // namespace guidemyride::models
+
+#endif // GUIDEMYRIDE_MODELS_DRIVER_H_
