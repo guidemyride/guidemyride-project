30de550444090a4c87b713b7010058dbb50939af
nyashbox <nyashbox@tutanota.com>
2024-10-19 15:17:28 +0300

    test(integration): add model tests stubs

---
+++ tests/integration/models/test_driver.cc
@@ -0,0 +1,14 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <guidemyride/models/driver.h>
+
+TEST(Driver, get) { GTEST_SKIP() << "Not implemented"; }
+
+TEST(Driver, verifyPassword) { GTEST_SKIP() << "Not implemented"; }
+
+TEST(Driver, updatePassword) { GTEST_SKIP() << "Not implemented"; }
+
+TEST(Driver, init) { GTEST_SKIP() << "Not implemented"; }
+
+TEST(Driver, commit) { GTEST_SKIP() << "Not implemented"; }
---
+++ tests/integration/models/test_session.cc
@@ -0,0 +1,8 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <guidemyride/models/session.h>
+
+TEST(Session, get) { GTEST_SKIP() << "Not implemented"; }
+
+TEST(Session, commit) { GTEST_SKIP() << "Not implemented"; }
