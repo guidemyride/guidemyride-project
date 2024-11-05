6b20f4f8467d3d5769d5019a1e625ee30e7dc92f
nyashbox <nyashbox@tutanota.com>
2024-11-05 03:29:05 +0200

    refactor(build): rewrite conanfile in python

---
+++ conanfile.py
@@ -0,0 +1,12 @@
+from conan import ConanFile
+
+class GuideMyRideRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("poco/1.13.3")
+        self.requires("grpc/1.65.0")
+        self.requires("argon2/20190702")
+        
+        self.test_requires("gtest/1.15.0")
