6b8bcbd72e11f8f5b2b06e3cd556dbd8f900f6c5
nyashbox <nyashbox@tutanota.com>
2024-11-05 15:13:12 +0200

    build(conan): use CMake layout

--- conanfile.py
+++ conanfile.py
@@ -10,3 +10,5 @@ class GuideMyRideRecipe(ConanFile):
         self.requires("argon2/20190702")
         
         self.test_requires("gtest/1.15.0")
+    def layout(self):
+        cmake_layout(self) 
