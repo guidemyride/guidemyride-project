64e06f1ee858964a94686c8085c30fe0f63d007c
nyashbox <nyashbox@tutanota.com>
2024-11-05 15:14:33 +0200

    build(conan): tune options for poco and grpc dependencies

--- conanfile.py
+++ conanfile.py
@@ -10,5 +10,43 @@ class GuideMyRideRecipe(ConanFile):
         self.requires("argon2/20190702")
         
         self.test_requires("gtest/1.15.0")
+
+    def configure(self):
+        poco = self.options["poco/*"]
+        grpc = self.options["grpc/*"]
+
+        # POCO options
+        poco.fPIC = True
+        poco.enable_net = True
+        poco.enable_xml = True
+        poco.enable_util = True
+        poco.enable_data = True
+        poco.enable_data_postgresql = True
+        poco.enable_redis = True
+        poco.enable_activerecord = True
+
+        poco.shared = False
+        poco.enable_jwt = False
+        poco.enable_zip = False
+        poco.enable_json = False
+        poco.enable_crypto = False
+        poco.enable_netssl = False
+        poco.enable_mongodb = False
+        poco.enable_data_mysql = False
+        poco.enable_data_sqlite = False
+
+        # gRPC options
+        grpc.fPIC = True
+        grpc.cpp_plugin = True
+
+        grpc.shared = False
+        grpc.php_plugin = False
+        grpc.node_plugin = False
+        grpc.ruby_plugin = False
+        grpc.csharp_plugin = False
+        grpc.python_plugin = False
+        grpc.with_libsystemd = False
+        grpc.objective_c_plugin = False
+
     def layout(self):
         cmake_layout(self) 
