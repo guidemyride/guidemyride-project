ceab57bf67a55bbde6b7e8ea0cff02be003873b3
nyashbox <nyashbox@tutanota.com>
2024-11-05 13:17:14 +0200

    chore(build): bump gRPC version

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class GuideMyRideRecipe(ConanFile):
 
     def requirements(self):
         self.requires("poco/1.13.3")
-        self.requires("grpc/1.65.0")
+        self.requires("grpc/1.67.1")
         self.requires("argon2/20190702")
         
         self.test_requires("gtest/1.15.0")
