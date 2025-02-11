d123f85a92c7e01c9c6ca403afbbe5440b25e5e9
nyashbox <nyashbox@tutanota.com>
2025-02-11 05:38:55 +0200

    refactor(libs/common): remove driver model declarations

--- libs/common/include/guidemyride/models/driver.h
+++ libs/common/include/guidemyride/models/driver.h
@@ -7,48 +7,12 @@
 
 #pragma once
 
-#include <guidemyride/models/session.h>
-#include <guidemyride/transaction/transaction.h>
-
-using guidemyride::transaction::TransactionBase;
-
 namespace guidemyride::models {
 
 class Driver {
   public:
-    Driver(const std::string &fullname, const std::string &password,
-           const std::string &phone);
-
-    template <typename T>
-    static Driver get(const T &value, TransactionBase &txn,
-                      const std::string_view field);
-
-    template <typename T>
-    static bool verifyPassword(const T &value, const std::string_view pass,
-                               TransactionBase &txn,
-                               const std::string_view field);
-
-    template <typename T>
-    static void updatePassword(const T &value, const std::string_view pass,
-                               TransactionBase &txn,
-                               const std::string_view field);
-
-    const std::string &getFullname(void) const;
-    const std::string &getPhoneNumber(void) const;
-    const std::string &getPassword(void) const;
-
-    static void init(TransactionBase &txn);
-    operator Session() const;
-    void commit(TransactionBase &txn);
-
+  protected:
   private:
-    Driver(const uint32_t id, const std::string &fullname,
-           const std::string &password, const std::string &phone);
-
-    uint32_t mId = 0;
-    std::string mFullname;
-    std::string mPassword;
-    std::string mPhone;
 };
 
 } // namespace guidemyride::models
