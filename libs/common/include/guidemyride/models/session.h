bbfde8bc811c6d537f14da7fe26d9c0d8b27c2f4
nyashbox <nyashbox@tutanota.com>
2025-02-11 05:38:40 +0200

    refactor(libs/common): remove session model declarations

--- libs/common/include/guidemyride/models/session.h
+++ libs/common/include/guidemyride/models/session.h
@@ -7,37 +7,12 @@
 
 #pragma once
 
-#include <map>
-#include <string>
-
-#include <guidemyride/transaction/transaction.h>
-#include <guidemyride/utils/hash.h>
-
-using guidemyride::transaction::TransactionBase;
-
 namespace guidemyride::models {
 
 class Session {
   public:
-    Session(const uint64_t ttl = 0);
-
-    static Session get(const std::string_view session_id,
-                       TransactionBase &conn);
-
-    const std::string &getSessionID(void) const;
-
-    std::string &operator[](const std::string &key);
-
-    void commit(TransactionBase &txn);
-
-    ~Session() = default;
-
+  protected:
   private:
-    Session(const std::string &session_id, const uint64_t ttl = 0);
-
-    std::string mSessionID;
-    std::map<std::string, std::string> mData;
-    uint64_t mTTL = 0;
 };
 
 } // namespace guidemyride::models
